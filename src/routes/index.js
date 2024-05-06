92d19b01a7d732f2269df386de1db2f6d36debc2
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-05-06 17:46:39 -0300

    starting routes and controllers

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express")
+
+const usersRoutes = require("./users.routes")
+/* const dishRoutes = require("./dish.routes")
+const sessionsRoutes = require("./sessions.routes") */
+
+const routes = Router()
+routes.use("/users", usersRoutes)
+/* routes.use("/dish", dishRoutes)
+routes.use("/sessions", sessionsRoutes) */
+
+module.exports = routes
