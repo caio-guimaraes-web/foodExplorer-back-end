940189401746f1890ad7a01ff77332234d664bd6
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-05-21 10:45:42 -0300

    creating Ingredients routs and controllers, finish back-end basics

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express")
 
 const usersRoutes = require("./users.routes")
 const dishRoutes = require("./dish.routes")
+const ingredientsRoutes = require("./ingredients.routes")
 /* const sessionsRoutes = require("./sessions.routes") */
 
 const routes = Router()
 routes.use("/users", usersRoutes)
 routes.use("/dish", dishRoutes)
+routes.use("/ingredients", ingredientsRoutes)
 /* routes.use("/sessions", sessionsRoutes) */
 
 module.exports = routes
